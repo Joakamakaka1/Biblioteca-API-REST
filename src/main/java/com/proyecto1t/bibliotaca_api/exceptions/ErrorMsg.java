6b2835f70586be0cac77097bd941ecc8704e3879
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2024-12-20 02:20:18 +0100

    Minimal changes

--- src/main/java/com/proyecto1t/bibliotaca_api/exceptions/ErrorMsg.java
+++ src/main/java/com/proyecto1t/bibliotaca_api/exceptions/ErrorMsg.java
@@ -1,5 +1,6 @@
 package com.proyecto1t.bibliotaca_api.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorMsg {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
     private int status;
     private List<String> errors;
